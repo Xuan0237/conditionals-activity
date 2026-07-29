a1ed7989f7ae1ce5365fcb7f2a0e815480a28ed2
Xuan0237 <202580237@psu.palawan.edu.ph>
2026-07-30 07:01:38 +0800

    Create practice1_4.cs

---
+++ practice1_4.cs
@@ -0,0 +1,6 @@
+int score = 78; // You can change this value to test different scores
+
+// Use the ternary operator to evaluate if the score is 50 or above
+string result = (score >= 50) ? "Passed" : "Failed";
+
+Console.WriteLine(result);
