39f95a4d204577e4a79acdacc10486a09d365a56
Xuan0237 <202580237@psu.palawan.edu.ph>
2026-07-30 06:59:09 +0800

    Create practice1(string).cs

---
+++ practice1(string).cs
@@ -0,0 +1,11 @@
+string firstName, lastName, fullName;
+
+Console.Write("Enter your first name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+lastName = Console.ReadLine();
+
+fullName = firstName + " " + lastName;
+
+Console.WriteLine("Full Name: " + fullName);
