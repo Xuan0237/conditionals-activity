0cb0a30f1d7cfcc641c7c56011ace7d1107f95ee
Xuan0237 <202580237@psu.palawan.edu.ph>
2026-07-30 07:00:23 +0800

    Create practice01_1.cs

---
+++ practice01_1.cs
@@ -0,0 +1,7 @@
+int number = 4; // You can change this value to test different numbers
+
+// Check if the number is even using the modulus operator (%)
+if (number % 2 == 0)
+    {
+        Console.WriteLine($"The number {number} is even.");
+    }
